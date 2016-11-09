5f2b0c4fc75502acce80763e649c4bac05e8254d
Sergio Vicente de las Heras <sergio.v.he@gmail.com>
2016-11-09 12:26:01 +0100

    Update multer configuration

--- src/multer.js
+++ src/multer.js
@@ -1,10 +1,15 @@
 ﻿const appRootPath = require('app-root-path');
 const config = require(appRootPath + '/config.js')
+const utils = require(appRootPath + '/utils.js');
 const multer = require('multer');
 
-exports.imageUpload = multer({
-    dest: config.multer.imgdest,
-    rename: function (fieldname, filename) {
-        return utils.hash(image.name);
+var imageStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, config.multer.imgdest)
+    },
+    filename: function (req, file, cb) {
+        cb(null, (utils.hash(file.fieldname) + '-' + Date.now()))
     }
 })
+
+exports.imageUpload = multer({ storage: imageStorage })
